10827a5d6830827db4bfd83563d46bdbb47a7b07
pruttymate <piritymaterobert@gmail.com>
2023-01-07 13:46:10 +0100

    Enemy és melee, WIP

---
+++ Ludum52/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int Hp = 100;
+    private int damage = 10;
+    public GameObject enemyObject;
+    // Update is called once per frame
+    void Update()
+    {
+        //viselkedés
+    }
+    public void getDamage(int dmg)
+    {
+        Hp -= dmg;
+        if (Hp <= 0)
+            Destroy(enemyObject);
+    }
+}
--- Ludum52/Assets/Scripts/meleeAttack.cs
+++ Ludum52/Assets/Scripts/meleeAttack.cs
@@ -7,7 +7,8 @@ public class meleeAttack : MonoBehaviour
     private bool attacking = false;
     private bool canAttack = true;
     private float attackDelay = 1f;
-    private float attackDuration = 0.1f;
+    private float attackDuration = 1f;
+    private int damage = 50;
     
     void Update()
     {
@@ -16,15 +17,33 @@ public class meleeAttack : MonoBehaviour
             StartCoroutine(attack());
         }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (attacking)
         {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                collision.gameObject.GetComponent<Enemy>().getDamage(damage);
+            }
             // sebzést ide írni.
+            attacking = false;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (attacking)
+        {
+            if(collision.gameObject.tag=="Enemy")
+            {
+                collision.gameObject.GetComponent<Enemy>().getDamage(damage);
+            }
+            // sebzést ide írni.
+            attacking = false;
         }
     }
     IEnumerator attack()
     {
+
         canAttack = false;
         attacking = true;
         yield return new WaitForSeconds(attackDuration);
