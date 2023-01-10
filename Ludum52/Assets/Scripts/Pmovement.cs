4baa1b0e0f7c81c6be2c29c71457f5791f8d5314
Milan8890 <milan.gyorgy.toth@gmail.com>
2023-01-10 19:57:33 +0100

    sprite flup rework, mozgás?
    van egy alternatív sor a mozgásban nem tudom melyik a jobb nézzétek meg és döntsük el

--- Ludum52/Assets/Scripts/Pmovement.cs
+++ Ludum52/Assets/Scripts/Pmovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Pmovement : MonoBehaviour
 {
-    private float Speed = 0.05f;
+    private float Speed = 0.1f;
     private float JumpPower = 7f;
     private float airSpeed = 1f;
 
@@ -15,9 +15,15 @@ public class Pmovement : MonoBehaviour
 
     float horMove = 0f;
     Rigidbody2D rb;
+
+    public List<GameObject> limbs;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        for(int i = 0; i < transform.Find("LIMBS").childCount; i++)
+            limbs.Add(transform.Find("LIMBS").GetChild(i).gameObject);
     }
 
     void Update()
@@ -28,19 +34,17 @@ public class Pmovement : MonoBehaviour
             airSpeed = 0.8f;
 
         horMove = Input.GetAxis("Horizontal") * Speed * airSpeed;
+        //horMove = Input.GetAxisRaw("Horizontal") * Speed * airSpeed; nem tudom eldönteni melyik a jobb
         
 
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded)
             rb.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
-            
     }
 
     private void FixedUpdate()
     {
-        if(!IsFacingRight)
-            GetComponent<SpriteRenderer>().flipX = true;
-        else
-            GetComponent<SpriteRenderer>().flipX = false;
+        foreach(GameObject limb  in limbs)
+            limb.GetComponent<SpriteRenderer>().flipX = !IsFacingRight;
 
         if (horMove > 0 && !IsFacingRight || horMove < 0 && IsFacingRight)
             IsFacingRight = !IsFacingRight;
